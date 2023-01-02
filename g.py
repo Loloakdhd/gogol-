e89743a6b4cbce2c0708022014f25f7eedec1585
VcvccJgjg <107493388+Loloakdhd@users.noreply.github.com>
2023-01-02 20:33:38 +0200

    Add files via upload

---
+++ g.py
@@ -0,0 +1,8 @@
+import time
+collor:2
+print('Запущен процес взлома ЯРДC РФ')
+time.sleep(2)
+for i in range(1, 10):
+  print("Процес, до конца взлома - {}%".format(i*5))
+  time.sleep(3)
+print("ЯРДС, успешно взломани!!!")
