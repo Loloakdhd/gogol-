1d992e6b3dc58dd9f60c1f383a42507d617275d3
VcvccJgjg <107493388+Loloakdhd@users.noreply.github.com>
2023-01-02 20:57:02 +0200

    Delete g.py

--- g.py
+++
@@ -1,8 +0,0 @@
-import time
-collor:2
-print('Запущен процес взлома ЯРДC РФ')
-time.sleep(2)
-for i in range(1, 10):
-  print("Процес, до конца взлома - {}%".format(i*5))
-  time.sleep(3)
-print("ЯРДС, успешно взломани!!!")
